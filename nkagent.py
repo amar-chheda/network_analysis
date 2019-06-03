bfae2944644ca45b8f959de0fbeb24bd7562f764
Amar Chheda <chheda.am@husky.neu.edu>
2019-06-03 09:07:09 -0400

    Comments added

--- nkagent.py
+++ nkagent.py
@@ -1,5 +1,13 @@
+"""
+this object takes in the NK model generated, the initial solution and the assignment, and gives back an aggent object.
+we also have probabiliy of risk taken in exploration and distance of exploration within the binary sequence.
+"""
+
+#import dependencies
 import numpy as np
 
+
+#initializing class
 class nkAgent:
 
     def __init__(self, model, iniSol, assign, risk=0.5, dist=1):
@@ -10,8 +18,12 @@ class nkAgent:
         self.score = model.cal_fit(self.sol)
         self.d = dist
         self.assign = assign
-
+    
     def mutate(self, alpha=1):
+    """
+    This func mutates the agent and returns the new set of solution and score.
+    alpha here suggests a constant multiplying factor to reduce the risk through each time-step. This helps with the convergence.
+    """
         tmp_sol = self.sol.copy()
         index = list(range(self.n))
         mut_ind = np.random.choice(index, size = self.d)
