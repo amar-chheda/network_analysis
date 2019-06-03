55258ce2b8f4f6b64467ce72bea2ab7437cf5f3c
Amar Chheda <chheda.am@husky.neu.edu>
2019-06-03 02:49:04 -0400

    comments added

--- landscapegenerator.py
+++ landscapegenerator.py
@@ -1,23 +1,33 @@
+"""
+This object takes in the value of N and K where N represents the number of components in the systems and K represents thier interaction
+Thus, using just two parameters the users can generate very smooth of complex landscapes.
+"""
+
+#importing dependency
 import numpy as np
 import itertools
 
 
+#initializing class
 class landscapeGenerator:
 
     def __init__(self, N, K):
-        self.n = N
-        self.k = K
+        self.n = N #number of components
+        self.k = K #complexity of interactions
         self.nk = self.nk_landscape()
         self.dep = self.dependency_structure()
         self.max = np.max(self.nk)
         self.min = np.min(self.nk)
 
+    #displays N the number of components
     def disp_n(self):
         return print(f'Value of N is : {self.n}')
-
+        
+    #displays K the complexity of interactions
     def disp_k(self):
         return print(f'Value of K is : {self.k}')
 
+    #creating the dependency structure for the NK landscape
     def dependency_structure(self):
         '''
         This function takes the number of elements and interdependency and
@@ -31,6 +41,7 @@ class landscapeGenerator:
 
         return dep
 
+    # the final landscape generator
     def nk_landscape(self):
         '''
         Generates an NK landscape - an array of random numbers ~U(0, 1).
@@ -39,6 +50,7 @@ class landscapeGenerator:
 
         return nk
 
+    #calculating the fitness of solution (searching through landscape the solution)
     def cal_fit(self, bit_str):
         '''
             Takes landscape and a combination and returns a vector of fitness
